43ee7b3acc218c07242e9a7e3e82c7178ad59747
jhollandus1 <jhollandus1a@gmail.com>
2026-09-22 17:37:04 -0400

    Added automatic UTC timestamp and tested (left-out) JS code to convert to local time.
    Also code added to enforce datetime2 property (defaulted to it anyway).

--- HeapProjectContext.cs
+++ HeapProjectContext.cs
@@ -3,4 +3,34 @@ using Microsoft.EntityFrameworkCore;
 public class HeapProjectContext(DbContextOptions<HeapProjectContext> options) : DbContext(options)
 {
     public virtual DbSet<HeapProject.Models.HeapHistoryItem> HeapHistoryItem { get; set; } = default!;
+
+    public override int SaveChanges()
+    {
+        UpdateTimestamp();
+        return base.SaveChanges();
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        UpdateTimestamp();
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    private void UpdateTimestamp()
+    {
+        var entries = ChangeTracker.Entries<HeapProject.Models.HeapHistoryItem>()
+            .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
+
+        foreach(var entry in entries)
+        {
+            entry.Entity.SavedTime = DateTime.UtcNow;
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<HeapProject.Models.HeapHistoryItem>()
+                        .Property(e => e.SavedTime)
+                        .HasColumnType("datetime2");
+    }
 }
